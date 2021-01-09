8322defc3c9c0ebee448d78b7e1ba44ce0950319
Hiroshi <af.18.715@gmail.com>
2021-01-09 14:17:47 +0900

    eslint追加

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    console.log("Hello, " + name + "!");
+}
+var myName = "TypeScript";
+sayHello(myName);
---
+++ js/main.ts
@@ -0,0 +1,10 @@
+/**
+ * あいさつ
+ * @param {string} name
+ */
+function sayHello(name: string):void {
+  console.log(`Hello, ${name}!`);
+}
+
+const myName = 'TypeScript';
+sayHello(myName);
